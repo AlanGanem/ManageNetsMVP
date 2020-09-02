3f5148a0308e4cb001fa71133390e48142a4ee89
unknown <ganem.alan@hotmail.com>
2020-09-02 15:24:08 -0300

    new repo structure

--- app_extensions/pandas_extensions.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def read_file(file_path):
-    try:
-        file = pd.read_csv(file_path)
-    except:
-        file = pd.read_excel(file_path)
-    return file
--- microblog.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from app.models import User, Process, Product, Link
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db':db, 'User':User, 'Product':Product, 'Process':Process, 'Link':Link}
-
-if __name__ == '__main__':
-    app.run(debug = True)
